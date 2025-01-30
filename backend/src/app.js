6b4b6af1cae576df088e45ff5278675887bb9982
Ankur3386 <ankurdeep03386@gmail.com>
2025-01-30 20:24:59 +0530

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const dotenv =require('dotenv')
+dotenv.config()
+const express =require('express')
+const app =express()
+const cors =require('cors')
+app.use(cors());
+//write now we are listening request from all wbsite as just for tetsing but when a real project than put the domain name in between brackets of  cors(write domain here)
+
+
+
+
+app.get('/home',(req,res)=>{
+    res.send("Hello ankur")
+})
+module.exports =app
