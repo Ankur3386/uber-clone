3367294ce3101b6f1335a77149c743d090284f94
Ankur3386 <ankurdeep03386@gmail.com>
2025-02-03 20:55:58 +0530

    registerUser and changes

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,13 +3,13 @@ const bcrypt =require('bcrypt')
 const jwt = require('jsonwebtoken')
 const userSchema =new mongoose.Schema(
     {
-    fullName:{
-        firstName:{
+    fullname:{
+        firstname:{
             type:String,
             required:true,
             minLength:[3,"First Name should be atleast of 3 length"]
         },
-        lastName:{
+        lastname:{
             type:String,
 
         }
@@ -39,7 +39,7 @@ userSchema.methods.generateAuthToken=function(){
 userSchema.methods.comparePassword= async function (password){
    return await  bcrypt.compare(password,this.password )
 }
-userSchema.statics.hashpassword =async function(){
+userSchema.statics.hashPassword =async function(){
     return await bcrypt.hash(password,10)
 }
 const userModel=mongoose.model(user,userSchema)
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,2 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const {body} =require('express-validator')
+const userController =require('../controllers/user.controller.js')
+router.post('/register',[
+    body('email').isEmail().withMessage('Invalid email'),
+    body('fullname.firstname').isLength({min:3}).withMessage('First name must be atleat 3  character long'),
+    body('password').isLength({min:6}).withMessage('password should be atlest of length 6')
+],userController.registerUser)
+module.exports =router;
