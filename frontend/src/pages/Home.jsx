89c1977e9225532321b1398bf418b3e30f60bee5
Ankur3386 <ankurdeep03386@gmail.com>
2025-02-10 20:58:08 +0530

    Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Home = () => {
   return (
     <div>
-        <div className='h-screen pt-8  w-full flex justify-between flex-col bg-red-400'>
+        <div className='  bg-cover bg-bottom bg-centre bg-[url(https://media.istockphoto.com/id/1226532587/photo/a-city-crossing-with-a-semaphore-red-light-in-semaphore.jpg?s=612x612&w=0&k=20&c=wFre7dWkGLPk7r3O9g9TMRYhEmbiMSG6bExXsUAw8v4=)] pt-8 h-screen w-full flex justify-between flex-col bg-red-400'>
             <img src="https://pluspng.com/img-png/uber-logo-vector-png-uber-icon-png-50-px-1600.png" alt="" className='w-16 ml-8' />
-            <div className='bg-white py-4 px-4'>
+            <div className='bg-white pb-7 py-4 px-4'>
                 <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
-                <button className='w-full bg-black text-white py-3 rounded mt-3'>Continue</button>
+                <Link to ='/login'className='  w-full bg-black flex items-center justify-center text-white py-3 rounded mt-5'>Continue</Link>
             </div>
         </div>
     </div>
